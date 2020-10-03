f431a93ca79bb3bbb636eba509dfc19ff91e22c8
ShreyanshPrakash <avishreyansh@gmail.com>
2020-10-03 16:45:27 +0530

    added cors package .

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require("path");
 const fs = require("fs");
+var cors = require('cors')
 
 const app = express();
 const PORT = 4200;
+
+
+
+app.use(cors())
 
 app.use(
     express.static(
